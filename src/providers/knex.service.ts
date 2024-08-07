d0a48886aba97e7a2b17ffb65580e089d9b9ebf9
muhammad <macbook_uz@Muhammads-Macbook.local>
2024-08-07 23:11:07 +0500

    docker

--- src/providers/knex.service.ts
+++ src/providers/knex.service.ts
@@ -1,25 +1,3 @@
-import knex, { Knex } from 'knex';
-
-export class KnexService {
-  instance: Knex;
-
-  constructor() {
-    this.instance = knex({
-      client: 'postgresql',
-      connection: {
-        host: 'john.db.elephantsql.com',
-        database: 'zrdldrse',
-        password: 'vW-QMd6vea2sG9HRJxwexLS8heaQVX97',
-        user: 'zrdldrse',
-      },
-      pool: {
-        min: 1,
-        max: 3,
-      },
-    });
-  }
-}
-
 // import knex, { Knex } from 'knex';
 
 // export class KnexService {
@@ -29,15 +7,37 @@ export class KnexService {
 //     this.instance = knex({
 //       client: 'postgresql',
 //       connection: {
-//         host: 'localhost',
-//         database: 'store',
-//         password: '5432',
-//         user: 'postgres',
+//         host: 'john.db.elephantsql.com',
+//         database: 'zrdldrse',
+//         password: 'vW-QMd6vea2sG9HRJxwexLS8heaQVX97',
+//         user: 'zrdldrse',
 //       },
 //       pool: {
-//         min: 2,
-//         max: 75,
+//         min: 1,
+//         max: 3,
 //       },
 //     });
 //   }
 // }
+
+import knex, { Knex } from 'knex';
+
+export class KnexService {
+  instance: Knex;
+
+  constructor() {
+    this.instance = knex({
+      client: 'postgresql',
+      connection: {
+        host: 'localhost',
+        database: 'podarkiuz',
+        password: '4324',
+        user: 'postgres',
+      },
+      pool: {
+        min: 2,
+        max: 75,
+      },
+    });
+  }
+}
