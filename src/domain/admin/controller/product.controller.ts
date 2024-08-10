aeabba29ffef46c99d9754df0fb81b0b938f8b26
muhammad <macbook_uz@Muhammads-Macbook.local>
2024-08-10 17:33:27 +0500

    pre-commit huskyrc config

--- src/domain/admin/controller/product.controller.ts
+++ src/domain/admin/controller/product.controller.ts
@@ -12,7 +12,6 @@ import { SetProductStatusDto } from '../dto/product-admin.dto';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { AdminGuard } from 'src/guard/admin.guard';
 import { ProductService } from 'src/domain/client/product/product.service';
-import { OrderListDto } from 'src/domain/orders/dto/order.dto';
 import { ListPageDto } from 'src/shared/dto/list.dto';
 import {
   CreateProductByAdminDto,
