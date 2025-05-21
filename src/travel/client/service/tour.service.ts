201b24ddc1c461ccf16402f42a908c5e6f96b652
muhammadabdumalikov <burakali424@gmail.com>
2025-05-21 23:06:59 +0500

    add locations list

--- src/travel/client/service/tour.service.ts
+++ src/travel/client/service/tour.service.ts
@@ -6,13 +6,14 @@ import { ILanguage, PaginationParams } from 'src/travel/shared/interfaces';
 import { FilesRepo } from 'src/travel/shared/repo/files.repo';
 import { ITourSeachByName } from 'src/travel/admin/interface/tour.interface';
 import { IGetTourListClient } from '../interface/tour.interface';
+import { LocationRepo } from 'src/travel/shared/repo/locations.repo';
 
 @Injectable()
 export class TourService {
   constructor(
     private readonly repo: TourRepo,
     private readonly cityRepo: CityRepo,
-    private readonly filesRepo: FilesRepo,
+    private readonly locationsRepo: LocationRepo,
   ) {}
 
   async getAllList(params: IGetTourListClient, lang: ILanguage) {
@@ -21,7 +22,12 @@ export class TourService {
   }
 
   async getCitiesList(params: PaginationParams) {
-    const data = await this.cityRepo.getAllCities(params);
+    const data = await this.cityRepo.getAllCitiesClient(params);
+    return data;
+  }
+
+  async getLocationsList(params: PaginationParams) {
+    const data = await this.locationsRepo.getAllLocationsClient(params);
     return data;
   }
 
